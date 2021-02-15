7c673c1a5606e2a16826582032ff9a48be2c1124
afif ahmed <afifahmed456123@gmail.com>
2021-02-15 22:39:26 +0530

    added form submissions using netlify forms

--- src/homepage/Hero.jsx
+++ src/homepage/Hero.jsx
@@ -186,7 +186,6 @@ export const Hero = () => {
                 color: '#fff',
               }}
               type='submit'
-              disabled
             >
               Send
             </button>
