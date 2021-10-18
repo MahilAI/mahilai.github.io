e1baeb74ae3b9e778bc351897a5e6d5173af5d12
mohammed afif ahmed <51887071+afif1400@users.noreply.github.com>
2021-10-18 15:23:28 +0530

    Updated about us content

--- src/About.jsx
+++ src/About.jsx
@@ -30,11 +30,10 @@ const About = () => {
 			<div className='aboutus'>
 				<h2>About Us</h2>
 				<p>
-					Welcome to MahilAI. We are a community of tech enthusiasts who want to
-					use our powers for good! MahilAI aims to use the powerful technologies
-					of Artificial Intelligence and Machine Learning to create solutions
-					for Violence Against Women. To this aim, we conduct workshops and have
-					a incubator process coming soon! Stay tuned!
+					Women are subjected to a lot of demeaning practices and societal negligence. In terms of numbers, the number of reports of domestic violence, sexual crimes, and child prostitution is shocking. Almost every woman has an account of molestation at different ages that have caused mental scars and trauma.
+MahilAI is an idea born out of these dark and corrosive roots of society today which are very often overlooked.
+AI technology can be used to combat atrocities against women, this is the sole belief of MahilAI. Ours is a community at the college level filled with concerned individuals, all set to make a difference and this world a safer place for women.
+MahilAI is an attempt to bring the AI revolution to destroy the perils faced by women and to create safe and empowering places for women. It is one step forward to challenge social stigmas, break gender barriers and make the world a better place to live in.
 				</p>
 			</div>
 		</div>
